ddaecc0e06c635f644396f40c3ccb9afab5ef0ee
aletorresrios <123060901+aletorresrios@users.noreply.github.com>
2023-10-31 12:48:24 +0100

    Añadido ejercicio de paginacion

--- springboot2/src/main/java/com/springboot/demo/SpringbootApplication.java
+++ springboot2/src/main/java/com/springboot/demo/SpringbootApplication.java
@@ -2,12 +2,19 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 
 }
