c15352398bd6b7a6a3d0c897c3fb28501a2d3faa
aletorresrios <123060901+aletorresrios@users.noreply.github.com>
2023-11-03 16:27:41 +0100

    Añadido actividad de eliminar y modificar alumnos

--- springboot2/src/main/java/com/springboot/demo/AlumnoController.java
+++ springboot2/src/main/java/com/springboot/demo/AlumnoController.java
@@ -5,6 +5,7 @@
 package com.springboot.demo;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,34 +14,67 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 /**
  *
- * @author Windows
- * parra mariquita
+ * @author Windows parra mariquita
  */
 @Controller
 public class AlumnoController {
+
     @Autowired
     private AlumnoRepository alumnorepository;
-    
+
     @PostMapping("/formulario")
-    public String guardarAlumno(Alumno alumno){
+    public String guardarAlumno(Alumno alumno) {
         alumnorepository.save(alumno);
         return "redirect:/";
     }
+
     @GetMapping("/")
-    public String listaPersonas(Model model,@PageableDefault(size=5) Pageable p) {
-        
+    public String listaPersonas(Model model, @PageableDefault(size = 5) Pageable p) {
+
         Page<Alumno> alumnos = alumnorepository.findAll(p);
         model.addAttribute("alumnos", alumnos);
         return "home";
     }
+
     @GetMapping("/formulario")
-    public String form(Model model){
+    public String form(Model model) {
         model.addAttribute("alumno", new Alumno());
         return "formulario";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarAlumno(@PathVariable Integer id) {
+        alumnorepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/editar/" + "{id}")
+            public String update(int id, Alumno a) {
+    //public String update(@PathVariable int id, @ModelAttribute Alumno alumnoActualizado) {
+        Optional<Alumno> alumnoExistente = alumnorepository.findById(id);
+        //alumnoExistente.ifPresent(value->alumnorepository.save(value));
+        
+        if (alumnoExistente.isPresent()) {
+            Alumno alumno = alumnoExistente.get();
+            alumno.setNombre(a.getNombre());
+            alumno.setApellido(a.getApellido());
+            alumno.setEdad(a.getEdad());
+            alumno.setDni(a.getDni());
+            alumnorepository.save(alumno);
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/" + "{id}")
+    public String update(@PathVariable Integer id, Model model) {
+        Optional<Alumno> alumno = alumnorepository.findById(id);
+        model.addAttribute("alumno", alumno);
+        return "modificar";
     }
 }
